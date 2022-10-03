efecc40a601348912e8e88d46eb0b5d2c0230050
BopMan102Terra3 <82306456+BopMan102Terra3@users.noreply.github.com>
2022-10-03 15:29:01 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('Posenet Is Initialized');
+}
+
+function draw()
+{
+    background('#87812e');
+    document.getElementById("font_side").innerHTML = "Font Size of the Text Will Be = " + difference + " px";
+    fill('#fa0584');
+    text('OCEAN GAWANDER', 50, 400);
+    textSize(difference);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+        }
+}
